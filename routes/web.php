5a82144517b2d6f3024ead60b2dd15e59b8e50b2
MSc Luis A. Hernández Elias <88979457+LuisHdezE@users.noreply.github.com>
2026-09-22 20:44:41 -0300

    U0.7 expose catalog administration and Swagger views

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('landing');
+Route::view('/catalogo', 'catalog')->name('catalog.admin');
+Route::view('/swagger', 'swagger')->name('swagger');
